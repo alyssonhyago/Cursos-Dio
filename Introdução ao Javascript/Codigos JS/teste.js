6a99fadfe82599c71b1fb73a70a94ca44aa6cf09
Alysson <alysson.oliveira@ee.ufcg.edu.br>
2022-01-17 17:03:30 -0300

    introdução ao JavaScript

---
+++ Introdução ao Javascript/Codigos JS/teste.js
@@ -0,0 +1,16 @@
+/*var desconto = 0.2;
+const PRECO = 2;
+preco = PRECO - desconto ;*/
+
+/*Funções
+function soma(a, b){
+    return a + b;
+}
+
+soma(2,4);*/
+
+//console.log('Helloo world !!');
+
+
+
+
