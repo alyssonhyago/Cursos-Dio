cc37d40adca520119324d8f8e9cee2daece4896e
Alysson <alysson.oliveira@ee.ufcg.edu.br>
2022-01-20 10:38:27 -0300

    tipos e variaveis

---
+++ Sintaxe básica em JavaScript/Aula 07 - Variáveis e Tipos/index.js
@@ -0,0 +1,20 @@
+
+
+/*var firstName = 'Alysson';
+
+if(firstName === 'Alysson'){
+    var firstName = 'Hyago';
+    let lastName = 'Pereria';
+    
+}*/
+
+//console.log();
+
+
+
+let nome = 'João';
+let sobrenome = 'Pereira';
+//let concatenado = nome.concat(sobrenome);
+let concatenado = nome + ' ' + sobrenome;
+console.log(concatenado.length);
+console.log(concatenado);
